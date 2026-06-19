f30b48fde693ebe1650c43fa828089aa2908348f
kndxhz <kndxhz@163.com>
2026-06-20 04:07:50 +0800

    feat: 过渡动画

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = '小伙纸的杂货间';
-export const SITE_DESCRIPTION = '很高兴见到你!';
+export const SITE_DESCRIPTION = '喵喵?喵喵喵?喵喵喵!';
